9ef02a675de60d5c209a7f9f0e97fa93e1c003ac
d-ryzhikov <d.ryzhykau@gmail.com>
2018-11-30 16:38:00 +0300

    Fix subscription topic

--- octoprint_mqtt_controlls/__init__.py
+++ octoprint_mqtt_controlls/__init__.py
@@ -9,8 +9,8 @@ CONTROLLS_TOPIC_NAME = 'octoprint/plugin/mqtt/controlls'
 class MQTTControllsPlugin(octoprint.plugin.StartupPlugin):
     def on_after_startup(self):
         helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_subscribe")
-        subscribe = helpers["mqtt_subscribe"]
-        subscribe("octoprint/plugin/mqtt_test/sub", self._on_mqtt_subscription)
+        subscribe = helpers.get("mqtt_subscribe")
+        subscribe(CONTROLLS_TOPIC_NAME, self._on_mqtt_subscription)
 
     def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
         self._logger.info("Received a control message {topic}: {message}".format(**locals()))
