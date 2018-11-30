29f123fd5afcd0deda81a4a7a5e565a12ae641a5
d-ryzhikov <d.ryzhykau@gmail.com>
2018-11-30 18:03:30 +0300

    Add connection logging

--- octoprint_mqtt_controls/__init__.py
+++ octoprint_mqtt_controls/__init__.py
@@ -8,12 +8,16 @@ CONTROLS_TOPIC_NAME = 'octoprint/plugin/mqtt/controls'
 
 class MQTTControlsPlugin(octoprint.plugin.StartupPlugin):
     def on_after_startup(self):
-        helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_subscribe")
-        subscribe = helpers.get("mqtt_subscribe")
-        subscribe(CONTROLS_TOPIC_NAME, self._on_mqtt_subscription)
+        helpers = self._plugin_manager.get_helpers('mqtt', 'mqtt_subscribe')
+        subscribe = helpers.get('mqtt_subscribe')
+        if subscribe:
+            self._logger.info('Connecting to mqtt broker...')
+            subscribe(CONTROLS_TOPIC_NAME, self._on_mqtt_subscription)
+        else:
+            self._logger.error("Could not retrieve 'mqtt_subscribe' helper.")
 
     def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
-        self._logger.info("Received a control message {topic}: {message}".format(**locals()))
+        self._logger.info("Received a control message - {topic}: {message}".format(**locals()))
 
 
 __plugin_name__ = 'MQTT Controls'
