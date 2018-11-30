28a564f6f2558e653b4562fdc056d19b23137b71
d-ryzhikov <d.ryzhykau@gmail.com>
2018-11-30 15:39:03 +0300

    Test version of plugin to log received control messages

---
+++ octoprint_mqtt-controlls/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+CONTROLLS_TOPIC_NAME = 'octoprint/plugin/mqtt/controlls'
+
+
+class MqttTestPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_subscribe")
+        subscribe = helpers["mqtt_subscribe"]
+        subscribe("octoprint/plugin/mqtt_test/sub", self._on_mqtt_subscription)
+
+    def _on_mqtt_subscription(self, topic, message, retained=None, qos=None, *args, **kwargs):
+        self._logger.info("Received a control message {topic}: {message}".format(**locals()))
+
+
+__plugin_implementations__ = [MqttTestPlugin()]
